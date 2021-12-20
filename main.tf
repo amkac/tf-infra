d125937ad522b209fb8e29b84ab482acf3cc70a6
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2021-12-20 20:15:28 +0100

    clean code

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-web-sg"
+  name = "tcp-sg-web"
   ingress {
     from_port   = 8080
     to_port     = 8080
