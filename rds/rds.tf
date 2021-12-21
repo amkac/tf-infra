1135a9d537a17720f1ae6705fdedffdfa7d8d041
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2021-12-21 21:09:36 +0100

    change db parameter group name

--- rds/rds.tf
+++ rds/rds.tf
@@ -4,16 +4,16 @@ resource "aws_db_subnet_group" "mariadb-subnet" {
   subnet_ids  = [var.PRIVATE_SUBNET_1_ID, var.PRIVATE_SUBNET_2_ID]
 }
 
-resource "aws_db_parameter_group" "mariadb-parameters" {
-  name        = "mariadb-parameters"
-  family      = "mariadb10.1"
-  description = "MariaDB parameter group"
+# resource "aws_db_parameter_group" "mariadb-parameters" {
+#   name        = "mariadb-parameters"
+#   family      = "mariadb10.1"
+#   description = "MariaDB parameter group"
 
-  parameter {
-    name  = "max_allowed_packet"
-    value = "16777216"
-  }
-}
+#   parameter {
+#     name  = "max_allowed_packet"
+#     value = "16777216"
+#   }
+# }
 
 resource "aws_db_instance" "mariadb" {
   allocated_storage       = 100 # 100 GB of storage, gives us more IOPS than a lower number
@@ -25,7 +25,7 @@ resource "aws_db_instance" "mariadb" {
   username                = "root"           # username
   password                = var.RDS_PASSWORD # password
   db_subnet_group_name    = aws_db_subnet_group.mariadb-subnet.name
-  parameter_group_name    = aws_db_parameter_group.mariadb-parameters.name
+  parameter_group_name    = "default.mariadb10.5"
   multi_az                = "false" # set to true to have high availability: 2 instances synchronized with each other
   vpc_security_group_ids  = [aws_security_group.allow-mariadb.id]
   storage_type            = "gp2"
