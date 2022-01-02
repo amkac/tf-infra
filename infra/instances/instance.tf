28d2c4c23d96754955c503e0e464d7daac9dd29c
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2022-01-02 22:28:20 +0100

    add instance

--- infra/instances/instance.tf
+++ infra/instances/instance.tf
@@ -1,27 +1,27 @@
 
 
-# resource "aws_instance" "web" {
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.allow-ssh.id, aws_security_group.allow-http.id]
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow-ssh.id, aws_security_group.allow-http.id]
 
-#   # the public SSH key
-#   key_name = aws_key_pair.mykey.key_name
+  # the public SSH key
+  key_name = aws_key_pair.mykey.key_name
 
-#   # the VPC subnet
-#   subnet_id = var.PUBLIC_SUBNET_ID
+  # the VPC subnet
+  subnet_id = var.PUBLIC_SUBNET_ID
 
-#   # user data
-#   user_data = data.template_cloudinit_config.cloudinit-example.rendered
+  # user data
+  user_data = data.template_cloudinit_config.cloudinit-example.rendered
 
-#   # user_data = <<-EOF
-#   #             #!/bin/bash
-#   #             apt-get install mysql-client
-#   #             echo "Hello, World" > index.html
-#   #             nohup busybox httpd -f -p 8080 &
-#   #             EOF
-# }
+  # user_data = <<-EOF
+  #             #!/bin/bash
+  #             apt-get install mysql-client
+  #             echo "Hello, World" > index.html
+  #             nohup busybox httpd -f -p 8080 &
+  #             EOF
+}
 
-# output "web-address" {
-#   value = aws_instance.web.public_dns
-# }
+output "web-address" {
+  value = aws_instance.web.public_dns
+}
--- infra/scripts/bootstrap.sh
+++ infra/scripts/bootstrap.sh
@@ -3,11 +3,10 @@
 sudo -s
 apt-get update
 apt  install awscli -y
-
+apt install mysql-client -y
 apt-get -y install net-tools nginx
 echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
 
 # echo "Hello, World" > index.html
 # nohup busybox httpd -f -p 8080 &
 
-apt install mysql-client -y
