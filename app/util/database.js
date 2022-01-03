cb697e4868d144dab3a77ab8885a80275f3333ce
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2022-01-03 21:05:02 +0100

    init app

---
+++ app/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'mariadb.c5scuymgh2ka.eu-west-1.rds.amazonaws.com',
+  user: 'root',
+  database: 'mariabd',
+  password: 'mypassword1993',
+});
+
+module.exports = pool.promise();
