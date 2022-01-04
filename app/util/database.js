33fdf73d09149a8367cd13c13d003cbc75bec572
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2022-01-04 10:01:48 +0100

    update connexion info

--- app/util/database.js
+++ app/util/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'mariadb.c5scuymgh2ka.eu-west-1.rds.amazonaws.com',
   user: 'root',
-  database: 'mariabd',
+  database: 'mariadb',
   password: 'mypassword1993',
 });
 
