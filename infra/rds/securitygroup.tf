62c946bd2a1e0438ae01fc1baf56bf2e0013192b
kacimimoghite <abdelmoghite.kacimi@capgemini.com>
2022-01-04 08:46:12 +0100

    update sg

--- infra/rds/securitygroup.tf
+++ infra/rds/securitygroup.tf
@@ -1,25 +1,25 @@
 
-resource "aws_security_group" "allow-mariadb" {
-  vpc_id      = var.VPC_ID
-  name        = "allow-mariadb"
-  description = "allow-mariadb"
-  ingress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    security_groups = var.ALLOWED_SG # allowing access from our example instance
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    self        = true
-  }
-  tags = {
-    Name = "allow-mariadb"
-  }
-}
+# resource "aws_security_group" "allow-mariadb" {
+#   vpc_id      = var.VPC_ID
+#   name        = "allow-mariadb"
+#   description = "allow-mariadb"
+#   ingress {
+#     from_port       = 3306
+#     to_port         = 3306
+#     protocol        = "tcp"
+#     security_groups = var.ALLOWED_SG # allowing access from our example instance
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     self        = true
+#   }
+#   tags = {
+#     Name = "allow-mariadb"
+#   }
+# }
 
 resource "aws_security_group" "allow-mariadb2" {
   vpc_id      = var.VPC_ID
@@ -30,6 +30,14 @@ resource "aws_security_group" "allow-mariadb2" {
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # allowing access from our example instance
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
   }
 
   tags = {
